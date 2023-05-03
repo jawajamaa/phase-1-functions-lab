ba0e6ba6f5fc0ecb8ff6b3a5a79e58bdd0039178
Timothy Ryon <tim@timryon.com>
2023-05-03 12:55:22 -0400

    Finished Scuber rides

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  if (someValue < 42) {
+return (42-someValue);
+  }
+  else { 
+    return (someValue-42);
+  }
+}
+
+  function distanceFromHqInFeet(someValue) {
+    if (someValue < 42) {
+      return (42-someValue)*264;
+        }
+        else { 
+          return (someValue-42)*264;
+        }
+      }
+
+  function distanceTravelledInFeet(start, destination) {
+    if (destination < start) {
+      return (start-destination)*264;
+        }
+        else { 
+          return (destination-start)*264;
+        }
+      }
+
+  function calculatesFarePrice(start, destination) {
+        if (distanceTravelledInFeet(start, destination) < 400) {
+          return 0;
+        }
+        else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination)-400)*0.02);
+      }
+      else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+      }
+      else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+      }
+    }
